2befa063ef2e47db3d468ed48d108d2c5c3f6d6e
MagicBot <MagicBot@magicbane.com>
2023-03-30 10:28:46 -0400

    Fidality borking mobs in buildings unborked.

--- src/engine/devcmd/cmds/InfoCmd.java
+++ src/engine/devcmd/cmds/InfoCmd.java
@@ -455,8 +455,7 @@ public class InfoCmd extends AbstractDevCmd {
 
 			output += newline;
 			output += "Speed : " + targetMob.getSpeed();
-			output += newline;
-			output += "Fidelity ID: " + targetMob.getFidalityID();
+
 			output += newline;
 			output += "EquipSet: " + targetMob.getEquipmentSetID();
 			output += newline;
