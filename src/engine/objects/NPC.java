ce8d5f0129e256e108cb7e57cc93af5c122b0988
MagicBot <MagicBot@magicbane.com>
2023-04-30 08:28:36 -0400

    Debug null value.

--- src/engine/objects/NPC.java
+++ src/engine/objects/NPC.java
@@ -170,6 +170,10 @@ public class NPC extends AbstractCharacter {
 			this.currentID = this.dbID;
 			this.setObjectTypeMask(MBServerStatics.MASK_NPC);
 			int contractID = rs.getInt("npc_contractID");
+
+			if (currentID == 15551)
+				this.runeSetID = 11;
+
 			this.parentZoneID = rs.getInt("parent");
 
 			this.gridObjectType = GridObjectType.STATIC;
