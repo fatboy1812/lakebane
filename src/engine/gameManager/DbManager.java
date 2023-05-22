07b337f3f158709cd858660cd90688f0318a889b
MagicBot <MagicBot@magicbane.com>
2023-05-22 10:32:15 -0400

    Pool size fixed

--- src/engine/gameManager/DbManager.java
+++ src/engine/gameManager/DbManager.java
@@ -298,7 +298,7 @@ public enum DbManager {
 
 		HikariConfig config = new HikariConfig();
 
-		config.setMaximumPoolSize(Runtime.getRuntime().availableProcessors() * 2);
+		config.setMaximumPoolSize(50);
 
 		config.setJdbcUrl("jdbc:mysql://" + ConfigManager.MB_DATABASE_ADDRESS.getValue() +
 				":" + ConfigManager.MB_DATABASE_PORT.getValue() + "/" +
