aaecc5eda2b727a0be88a674dca0b51b57cbc6a1
FatBoy-DOTC <justin.chucksinsulating@gmail.com>
2024-12-28 06:05:19 -0600

    bane commander NPC

--- src/engine/objects/Bane.java
+++ src/engine/objects/Bane.java
@@ -284,10 +284,13 @@ public final class Bane {
         {
             baneCommander = NPC.getNPC(commanderuuid);
         }
+        baneCommander.runAfterLoad();
         baneCommander.setLoc(spawnLoc);
         InterestManager.setObjectDirty(baneCommander);
         WorldGrid.addObject(baneCommander,spawnLoc.x, spawnLoc.z);
         WorldGrid.updateObject(baneCommander);
+
+        baneCommander.updateLocation();
     }
 
     public static Bane getBane(int cityUUID) {
