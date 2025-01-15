86d7233aa4e80f887b174878c0a22c52fc472442
FatBoy-DOTC <justin.chucksinsulating@gmail.com>
2025-01-15 16:23:10 -0600

    remove console spam

--- src/engine/server/login/LoginServer.java
+++ src/engine/server/login/LoginServer.java
@@ -450,7 +450,7 @@ public class LoginServer {
                 objectUUID = rs.getInt("UID");
                 objectType = rs.getString("type");
 
-                Logger.info("INVALIDATED : " + objectType + " UUID: " + objectUUID);
+                //Logger.info("INVALIDATED : " + objectType + " UUID: " + objectUUID);
 
                 switch (objectType) {
 
@@ -462,7 +462,7 @@ public class LoginServer {
                         PlayerCharacter player = (PlayerCharacter) DbManager.getObject(Enum.GameObjectType.PlayerCharacter, objectUUID);
                         PlayerCharacter.initializePlayer(player);
                         player.getAccount().characterMap.replace(player.getObjectUUID(), player);
-                        Logger.info("Player active state is : " + player.isActive());
+                        //Logger.info("Player active state is : " + player.isActive());
                         break;
                 }
 
