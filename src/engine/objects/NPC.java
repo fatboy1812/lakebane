b0dd7e9b592af539f354d80ee71af0f2bb95a29d
FatBoy-DOTC <justin.chucksinsulating@gmail.com>
2025-02-18 21:37:43 -0600

    bug fix

--- src/engine/objects/NPC.java
+++ src/engine/objects/NPC.java
@@ -876,9 +876,10 @@ public class NPC extends AbstractCharacter {
         // zone collection
 
         this.parentZone = ZoneManager.getZoneByUUID(this.parentZoneUUID);
-        this.parentZone.zoneNPCSet.remove(this);
-        this.parentZone.zoneNPCSet.add(this);
-
+        if(this.parentZone != null) {
+            this.parentZone.zoneNPCSet.remove(this);
+            this.parentZone.zoneNPCSet.add(this);
+        }
         // Setup location for this NPC
 
         this.bindLoc = new Vector3fImmutable(this.statLat, this.statAlt, this.statLon);
