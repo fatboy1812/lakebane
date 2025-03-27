aac0ef2616c12e70b6731e0d8c5d9fd11fe44aaa
FatBoy-DOTC <justin.chucksinsulating@gmail.com>
2025-03-26 19:35:38 -0500

    new forge manager boiler plate

---
+++ src/engine/gameManager/ForgeManager.java
@@ -0,0 +1,15 @@
+package engine.gameManager;
+
+import engine.objects.ForgeWorkOrder;
+import engine.objects.NPC;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ForgeManager {
+    public HashMap<NPC, ArrayList<ForgeWorkOrder>> work_orders;
+
+    public static void add_work_order(NPC npc, ForgeWorkOrder workOrder){
+
+    }
+}
---
+++ src/engine/objects/ForgeWorkOrder.java
@@ -0,0 +1,11 @@
+package engine.objects;
+
+public class ForgeWorkOrder {
+    public int itembaseUUID;
+    public String prefix;
+    public String suffix;
+    public boolean resourceRolled;
+    public boolean randomRolled;
+    public int orderAmount;
+    public int slot;
+}
