e81326b3c2d71cfda4ffc57ed92bd45d9f5149cc
FatBoy-DOTC <justin.chucksinsulating@gmail.com>
2025-04-08 18:59:50 -0500

    mob type system

--- src/engine/mobileAI/SuperSimpleMobAI.java
+++ src/engine/mobileAI/SuperSimpleMobAI.java
@@ -5,32 +5,76 @@ import engine.mobileAI.MobHandlers.PetHandler;
 import engine.mobileAI.MobHandlers.PlayerGuardHandler;
 import engine.mobileAI.MobHandlers.SiegeHandler;
 import engine.objects.Mob;
+import org.pmw.tinylog.Logger;
 
 public class SuperSimpleMobAI {
 
     public static void run(Mob mob){
 
-        if(mob.isMoving())
-            mob.updateLocation();
+        try {
+            MobType type = MobType.getMobType(mob);
+            switch(type){
+                case Siege:
+                    SiegeHandler.run(mob);
+                    break;
+                case Standard:
+                    MobHandler.run(mob);
+                    break;
+                case PlayerGuard:
+                    PlayerGuardHandler.run(mob);
+                    break;
+                case Pet:
+                    PetHandler.run(mob);
+                    break;
+            }
 
-        if(mob.getMobBaseID() == 13171){
-            SiegeHandler.run(mob);
-            return;
+
+            if (mob.isMoving())
+                mob.updateLocation();
+
+            if (mob.getMobBaseID() == 13171) {
+                SiegeHandler.run(mob);
+                return;
+            }
+            if (mob.isPet() && !mob.isSiege()) {
+                //MobAI.DetermineAction(mob);
+                PetHandler.run(mob);
+                return;
+            } else if (mob.isSiege()) {
+                SiegeHandler.run(mob);
+                //MobAI.DetermineAction(mob);
+                return;
+            } else if (mob.isPlayerGuard()) {
+                PlayerGuardHandler.run(mob);
+                //MobAI.DetermineAction(mob);
+                return;
+            } else {
+                MobHandler.run(mob);
+            }
+        }catch(Exception e){
+            Logger.error(e.getMessage());
         }
-        if(mob.isPet() && !mob.isSiege()) {
-            //MobAI.DetermineAction(mob);
-            PetHandler.run(mob);
-            return;
-        }else if (mob.isSiege()) {
-            SiegeHandler.run(mob);
-            //MobAI.DetermineAction(mob);
-            return;
-        }else if(mob.isPlayerGuard()){
-            PlayerGuardHandler.run(mob);
-            //MobAI.DetermineAction(mob);
-            return;
-        }else {
-            MobHandler.run(mob);
+    }
+    public enum MobType{
+        PlayerGuard,
+        Standard,
+        Pet,
+        Siege;
+
+        public static MobType getMobType(Mob mob){
+            if (mob.getMobBaseID() == 13171) {
+                return Siege;
+            }
+            if (mob.isPet() && !mob.isSiege()) {
+                return Pet;
+            } else if (mob.isSiege()) {
+                return Siege;
+            } else if (mob.isPlayerGuard()) {
+                return PlayerGuard;
+            } else {
+                return Standard;
+            }
         }
     }
 }
+
